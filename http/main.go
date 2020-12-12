9d3c795d694fc6a6b3c2c491fcbf0e1806ae8c32
Benjamin Bennett <synaptology@gmail.com>
2020-12-12 16:50:54 +0000

    Moving http directory

--- http/main.go
+++ http/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/bendbennett/go-bits/concurrency/http/request"
+	"github.com/bendbennett/go-bits/http/request"
 	"net/http"
 	"runtime"
 	"time"
